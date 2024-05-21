b7e3e7ca2154fc267aec349df20360cc061adc22
Brian Sauls <brianstein27@proton.me>
2024-05-21 12:55:11 +0200

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,12 @@ Rails.application.routes.draw do
   # get 'uniqorns/index'
   devise_for :users
   root to: "pages#home"
-  ressources :uniqorns
-    ressources :users
+  resources :uniqorns, only: %i[show index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-
 
   # get "uniqorns", to: "uniqorns#index"
   # get "uniqorns/:id", to: "uniqorns#show"
