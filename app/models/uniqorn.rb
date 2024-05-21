27788aaf2d0c5e2108a332d27fdfb63dfab283f5
Brian Sauls <brianstein27@proton.me>
2024-05-21 13:11:48 +0200

    Fix typos and padding

--- app/models/uniqorn.rb
+++ app/models/uniqorn.rb
@@ -2,6 +2,6 @@ class Uniqorn < ApplicationRecord
   belongs_to :user
   has_many :reviews, through: :bookings
 
-  validates :name, presence: true, uniquness: true, length: { min: 3 }
+  validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
   validates :price_per_day, presence: true
 end
