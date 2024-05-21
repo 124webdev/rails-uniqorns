027f5a0112dcd2e49551b618c5b7af2a0b07f5cc
Franziska Horn <Franziska_Horn@hotmail.com>
2024-05-21 14:49:26 +0200

    Added delete to seeding file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,24 @@
 
-# Uniqorn.create([
-#   {
-#     name: "Twilight Sparkle",
-#     age: 5,
-#     can_fly: true,
-#     color: "Lavender",
-#     speed: 90,
-#     price_per_day: 350,
-#     user_id: 1
-#   },
-#   {
-#     name: "Rainbow Dash",
-#     age: 6,
-#     can_fly: true,
-#     color: "Sky Blue",
-#     speed: 120,
-#     price_per_day: 200,
-#     user_id: 1
-#   },
-# ])
+Uniqorn.delete_all
+puts "Deleted all existing uniqorn records."
+
+Uniqorn.create([
+  {
+    name: "Twilight Sparkle",
+    age: 5,
+    can_fly: true,
+    color: "Lavender",
+    speed: 90,
+    price_per_day: 350,
+    user_id: 1
+  },
+  {
+    name: "Rainbow Dash",
+    age: 6,
+    can_fly: true,
+    color: "Sky Blue",
+    speed: 120,
+    price_per_day: 200,
+    user_id: 1
+  },
+])
