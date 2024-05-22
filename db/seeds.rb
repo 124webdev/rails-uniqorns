205ff885a0c1d0c31b5700783271bc735b6ff873
Franziska Horn <Franziska_Horn@hotmail.com>
2024-05-22 11:57:48 +0200

    Adaption in the seeding file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,14 @@
-
-Uniqorn.delete_all
+Uniqorn.destroy_all
 puts "Deleted all existing uniqorn records."
+User.destroy_all
+puts "Deleted all existing users."
 
-Uniqorn.create([
+user = User.create!(
+  email: "max@max.com",
+  password: "123456"
+)
+
+Uniqorn.create!([
   {
     name: "Twilight Sparkle",
     age: 5,
@@ -10,7 +16,7 @@ Uniqorn.create([
     color: "Lavender",
     speed: 90,
     price_per_day: 350,
-    user_id: 1
+    user_id: user.id
   },
   {
     name: "Rainbow Dash",
@@ -19,6 +25,6 @@ Uniqorn.create([
     color: "Sky Blue",
     speed: 120,
     price_per_day: 200,
-    user_id: 1
+    user_id: user.id
   },
 ])
