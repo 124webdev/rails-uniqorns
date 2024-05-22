c524d87ebbd77b85e44c6055e66793c31da122cd
Brian Sauls <brianstein27@proton.me>
2024-05-22 11:27:48 +0200

    Fixed review model relations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :booking
-  belongs_to :user, through: :booking
-  belongs_to :user, through: :uniqorn
+  has_one :user, through: :bookings
+  has_one :user, through: :uniqorns
 
   validates :content, presence: true
   validates :rating, presence: true
