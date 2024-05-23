e7dc4642c8e5c57b405e2100ed3b0267ce1ca9b1
Alberto Zech <albertozech@outlook.de>
2024-05-23 15:58:54 +0200

    delete index and show

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,4 @@
 class ReviewsController < ApplicationController
-  def index
-    @reviews = Review.all
-  end
-
-  def show
-    @review = Review.find(params[:id])
-  end
 
   def new
     @booking = Booking.find(params[:booking_id])
