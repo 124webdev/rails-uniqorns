36f94fa71472d85738326525939f37e53ec073ca
Precious Kanu <ifechukwukanu@gmail.com>
2024-05-23 16:53:57 +0200

    added xtra validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,6 +4,6 @@ class Booking < ApplicationRecord
 
   enum status: [ :pending, :rejected, :accepted ]
 
-  validates :start_date, presence: true
+  validates :start_date, presence: true, comparison: { greater_than_or_equal_to: Date.today, message: "cannot be in the past, please fill in a valid date!" }
   validates :end_date, presence: true
 end
